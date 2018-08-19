22a75fa413b43dfc2e6db4b0cb91d02bd488cb12
Sachin Dubey <sachin3939sd@gmail.com>
2018-08-19 15:20:46 +0530

    Update next20leap.py

--- next20leap.py
+++ next20leap.py
@@ -1,14 +1,14 @@
 def isLeap(year):
     return((year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0))
 
-a = int(input("Enter a year:\n"))
+year = int(input("Enter a year:\n"))
 leap = []
-if(isLeap(a)):
-    b = a
+if(isLeap(year)):
+    next = year
     while (len(leap) != 20):
-        b = b + 4 
-        if(isLeap(b)):
-            leap.append(b)
+        next = next + 4 
+        if(isLeap(next)):
+            leap.append(next)
         else:
             print("Entered year is not a leap year.")
 print(leap)
